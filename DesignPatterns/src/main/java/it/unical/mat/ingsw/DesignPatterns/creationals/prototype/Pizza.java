ef0cecf1fa4d2c55e5b9205bf265817fa941961f
cosmog97 <cosmog97@gmail.com>
2019-11-02 18:35:07 +0100

    Test

--- DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/creationals/prototype/Pizza.java
+++
@@ -1,8 +0,0 @@
-package it.unical.mat.ingsw.DesignPatterns.creationals.prototype;
-
-public abstract class Pizza implements Cloneable {
-	@Override
-    public Object clone() throws CloneNotSupportedException{
-        return super.clone();
-    }
-}
--- DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/creationals/prototype/PizzaNapoletana.java
+++
@@ -1,5 +0,0 @@
-package it.unical.mat.ingsw.DesignPatterns.creationals.prototype;
-
-public class PizzaNapoletana {
-
-}
