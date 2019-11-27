95b9063728d8291ab53df0fdbf1f2823519457f6
cosmog97 <cosmog97@gmail.com>
2019-11-27 18:30:58 +0100

    TemplateMethod added

---
+++ DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/behaviorals/templatemethod/second/CSVTable.java
@@ -0,0 +1,24 @@
+package it.unical.mat.ingsw.DesignPatterns.behaviorals.templatemethod.second;
+
+import java.util.List;
+
+public class CSVTable extends Table {
+
+	@Override
+	protected StringBuilder doBody(List<DVD> dvdList) {
+		StringBuilder csv = new StringBuilder();
+		csv.append("TITOLO;REGIA;GENERE" + "\n");
+		for (DVD dvd : dvdList) {
+			csv.append(dvd.getTitolo() + ";");
+			csv.append(dvd.getRegia() + ";");
+			csv.append(dvd.getGenere());
+			csv.append("\n");
+		}
+		return csv;
+	}
+
+	@Override
+	protected void doHook() {
+		this.filePath += ".csv";
+	}
+}
---
+++ DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/behaviorals/templatemethod/second/XMLTable.java
@@ -0,0 +1,26 @@
+package it.unical.mat.ingsw.DesignPatterns.behaviorals.templatemethod.second;
+
+import java.util.List;
+
+public class XMLTable extends Table {
+
+	@Override
+	public StringBuilder doBody(List<DVD> dvdList) {
+		StringBuilder xml = new StringBuilder();
+		xml.append("<DVD_LIST>" + "\n");
+		for (DVD dvd : dvdList) {
+			xml.append(" <DVD>" + "\n");
+			xml.append("  <TITOLO>" + dvd.getTitolo() + "</TITOLO>" + "\n");
+			xml.append("  <REGIA>" + dvd.getRegia() + "</REGIA>" + "\n");
+			xml.append("  <GENERE>" + dvd.getGenere() + "</GENERE>" + "\n");
+			xml.append(" </DVD>" + "\n");
+		}
+		xml.append("</DVD_LIST>" + "\n");
+		return xml;
+	}
+
+	@Override
+	protected void doHook() {
+		this.filePath += ".xml";
+	}
+}
