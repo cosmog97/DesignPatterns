81ff011b2501b17e1732ae5eeb35f7d60c213bc4
cosmog97 <cosmog97@gmail.com>
2019-11-30 10:57:50 +0100

    Expression added

---
+++ DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/behaviorals/interpreter/third/Expression.java
@@ -0,0 +1,5 @@
+package it.unical.mat.ingsw.DesignPatterns.behaviorals.interpreter.third;
+
+public abstract class Expression {
+	abstract public boolean interpret(String str);
+}
---
+++ DesignPatterns/src/main/java/it/unical/mat/ingsw/DesignPatterns/behaviorals/interpreter/third/TerminalExpression.java
@@ -0,0 +1,25 @@
+package it.unical.mat.ingsw.DesignPatterns.behaviorals.interpreter.third;
+
+import java.util.StringTokenizer;
+
+public class TerminalExpression extends Expression {
+
+	private String literal = null;
+
+	public TerminalExpression(String str) {
+		literal = str;
+	}
+
+	@Override
+	public boolean interpret(String str) {
+		StringTokenizer st = new StringTokenizer(str);
+		while (st.hasMoreTokens()) {
+			String test = st.nextToken();
+			if (test.equals(literal)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
